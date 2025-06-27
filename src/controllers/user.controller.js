ee4a259a31210138603b56d4b4574d521e426e72
Mokwa Moffat <moffatmkwa12@gmail.com>
2025-06-27 22:47:25 +0300

    add user controller with CRUD functionalities

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,52 @@
+
+const users = [];
+
+const getUsers = (req, res) => {
+    res.json(users);
+};
+
+const getUser = (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).send('User not found');
+    }
+};
+
+const createUser = (req, res) => {
+    const user = {
+        id: users.length + 1,
+        name: req.body.name
+    };
+    users.push(user);
+    res.status(201).json(user);
+};
+
+const updateUser = (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    if (user) {
+        user.name = req.body.name;
+        res.json(user);
+    } else {
+        res.status(404).send('User not found');
+    }
+};
+
+const deleteUser = (req, res) => {
+    const userIndex = users.findIndex(u => u.id === parseInt(req.params.id));
+    if (userIndex !== -1) {
+        users.splice(userIndex, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).send('User not found');
+    }
+};
+
+module.exports = {
+    getUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser
+};
