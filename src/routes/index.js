775809adb3928f44c5a26235c2ae5a463cd62698
Mokwa Moffat <moffatmkwa12@gmail.com>
2025-06-27 22:32:21 +0300

    setup route index to manage user, product, auth, and cart routers

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+
+const userRouter = require('./user.routes');
+const productRouter = require('./product.routes');
+const authRouter = require('./auth.routes');
+const cartRouter = require('./cart.routes');
+
+module.exports = {
+    userRouter,
+    productRouter,
+    authRouter,
+    cartRouter
+};
