3f5d221a17fabb095fa75683aaa650724d1b67a5
Mokwa Moffat <moffatmkwa12@gmail.com>
2025-06-27 22:45:44 +0300

    add index file to export controllers

---
+++ src/controllers/index.js
@@ -0,0 +1,12 @@
+
+const userController = require('./user.controller');
+const productController = require('./product.controller');
+const authController = require('./auth.controller');
+const cartController = require('./cart.controller');
+
+module.exports = {
+    userController,
+    productController,
+    authController,
+    cartController
+};
